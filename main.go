7685c5bb7279dfa3a5d51ea7b183ef58b64ddf9e
John Weldon <johnweldon4@gmail.com>
2015-12-02 11:46:38 -0700

    Swift Check

--- main.go
+++ main.go
@@ -1,16 +1,93 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/ncw/swift"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
-}
+var (
+	OS_VARIABLES = []string{
+		"OS_API_KEY",
+		"OS_AUTH_URL",
+		"OS_REGION_NAME",
+		"OS_TENANT_ID",
+		"OS_TENANT_NAME",
+		"OS_USERNAME",
+		"OS_PASSWORD",
+		"OS_CONTAINER_NAME",
+	}
+)
 
 func main() {
-    http.HandleFunc("/", defaultHandler)
-    fmt.Println("Example app listening at http://localhost:8888")
-    http.ListenAndServe(":8888", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/swifttest", swifttest)
+	mux.HandleFunc("/", index)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":8888")
+}
+
+type indexData struct {
+	Title     string
+	Heading   string
+	Action    string
+	Variables []string
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("index").Parse(indexTemplate))
+	d := indexData{
+		Title:     "OpenStack test",
+		Heading:   "OpenStack swift variables",
+		Action:    "swifttest",
+		Variables: OS_VARIABLES,
+	}
+	t.Execute(w, d)
+}
+
+type swiftData struct {
+	Success bool
+	Error   error
+	Items   []string
+	Vars    map[string]string
+}
+
+func swifttest(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.New("swifttest").Parse(swifttestTemplate))
+	d := swiftData{}
+
+	vars := make(map[string]string)
+	for _, k := range OS_VARIABLES {
+		vars[k] = r.PostFormValue(k)
+	}
+	d.Vars = vars
+
+	if conn, err := getSwiftConnection(vars); err != nil {
+		d.Error = err
+	} else {
+		if items, err := conn.ObjectNames(vars["OS_CONTAINER_NAME"], nil); err != nil {
+			d.Error = err
+		} else {
+			d.Items = items
+		}
+	}
+	t.Execute(w, d)
+}
+
+func getSwiftConnection(vars map[string]string) (*swift.Connection, error) {
+	c := swift.Connection{
+		UserName: vars["OS_USERNAME"],
+		ApiKey:   vars["OS_API_KEY"],
+		AuthUrl:  vars["OS_AUTH_URL"],
+		Region:   vars["OS_REGION_NAME"],
+		Tenant:   vars["OS_TENANT_NAME"],
+		TenantId: vars["OS_TENANT_ID"],
+	}
+	if err := c.Authenticate(); err != nil {
+		return nil, err
+	}
+	return &c, nil
 }
---
+++ templates.go
@@ -0,0 +1,50 @@
+package main
+
+const (
+	indexTemplate = `
+<html><head>
+	<title>{{ .Title }}</title>
+</head><body>
+	<div>
+		<h1>Swift Credential Test</h1>
+		<form action='{{ .Action }}' method='post' id='testform' target='_self'>
+			<fieldset>
+				<legend>{{ .Heading }}</legend>
+				{{ range .Variables }}<p>
+					<label for='{{ . }}'>{{ . }}</label><br />
+					<input name='{{ . }}' type='text' required />
+				</p>
+				{{ end }}
+
+			</fieldset>
+			<button>Submit</button>
+		</form>
+	</div>
+</body></html>
+`
+	swifttestTemplate = `
+<html><head>
+	<title>{{ if .Success }}It Works!{{ else }}Problem :({{ end }}</title>
+</head><body>
+	<div>
+	<h1>{{ if .Success }}It Works!{{ else }}There was a problem{{ end }}</h1>
+	{{ with .Error }}<span class='error'>{{ . }}</span>{{ end }}
+	{{ with .Vars }}
+	<table>
+		<tr><th>Key</th><th>Value</th></tr>
+		{{ range $k, $v := . }}<tr><td>{{ $k }}</td><td>{{ $v }}</td></tr>
+		{{ end }}
+	</table>
+	{{ end }}
+
+	{{ with .Items }}
+	<h3>Found</h3>
+	<ul>
+		{{ range . }}<li>{{ . }}</li>{{ end }}
+	</ul>
+	{{ end }}
+
+	</div>
+</body></html>
+`
+)
